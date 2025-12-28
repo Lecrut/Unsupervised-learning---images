1375ad114207a986541508a35aceea84e99d19d9
Lecrut <filip.lecrut12@gmail.com>
2025-12-28 18:57:29 +0100

    empty gui

---
+++ gui.py
@@ -0,0 +1,8 @@
+#%% Imports 
+import streamlit as st
+
+#%% GUI Layout
+st.title("Welcome to the Streamlit App")
+st.write("This is a simple GUI application built with Streamlit.")
+st.button("Click Me!")
+
